9c522b0f68a388e088ef4354375725f914284677
duantp <duantp@vimo.vn>
2021-05-18 10:15:22 +0700

    [18.05.2021] @CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders = "*")

--- src/main/java/com/nuce/duantp/sunshine/controller/SunShineController.java
+++ src/main/java/com/nuce/duantp/sunshine/controller/SunShineController.java
@@ -4,6 +4,7 @@ import com.nuce.duantp.sunshine.dto.response.NewsRes;
 import com.nuce.duantp.sunshine.dto.response.PageHomeRes;
 import com.nuce.duantp.sunshine.service.SunShineService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/sunshine")
+@CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders = "*")
 public class SunShineController {
     @Autowired
     private SunShineService sunShineService;
