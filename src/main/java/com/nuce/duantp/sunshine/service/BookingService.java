da3e6c0ec91327e19fe035adedc1429eddf6be8b
duantp <duantp@vimo.vn>
2021-06-22 22:51:31 +0700

    update status cancel booking

--- src/main/java/com/nuce/duantp/sunshine/service/BookingService.java
+++ src/main/java/com/nuce/duantp/sunshine/service/BookingService.java
@@ -138,7 +138,7 @@ public class BookingService {
         if (date.getHours() >= 8 && date.getHours() < 23) {
             tbl_Booking booking = new tbl_Booking
                     (bookingId, customer.get().getEmail(), date, bookingReq.getTotalSeats(), deposit.getDepositId(),
-                            0, tableName,bookingReq.getNote());
+                            1, tableName,bookingReq.getNote());
             List<tbl_Booking> data92 = bookingRepository.findAll();
 
             bookingRepository.save(booking);
